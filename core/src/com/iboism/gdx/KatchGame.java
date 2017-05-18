c10f4d76ce671ef161f56904875ac683f55b579e
Brad <brad.iboism@gmail.com>
2017-05-18 17:26:53 -0500

    finished implementing katch with libgdx

--- core/src/com/iboism/gdx/KatchGame.java
+++ core/src/com/iboism/gdx/KatchGame.java
@@ -2,6 +2,7 @@ package com.iboism.gdx;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -10,14 +11,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.utils.Array;
 
-public class KatchGame extends ApplicationAdapter implements GestureDetector.GestureListener {
+public class KatchGame extends ApplicationAdapter implements InputProcessor {
 	private AssetManager assets;SpriteBatch batch;
 
+	private boolean isLeftThusting;
+	private boolean isRightThrusting;
 
 	private TextureAtlas kshipAtlas;
 	private Array<TextureAtlas.AtlasRegion> kshipSprites;
+	private TextureAtlas.AtlasRegion sprite_lr;
+	private TextureAtlas.AtlasRegion sprite_l;
+	private TextureAtlas.AtlasRegion sprite_r;
+	private TextureAtlas.AtlasRegion sprite_n;
+	private TextureAtlas.AtlasRegion sprite_current;
 	private float elapsed;
 	OrthographicCamera camera;
 
@@ -25,9 +34,10 @@ public class KatchGame extends ApplicationAdapter implements GestureDetector.Ges
 	float game_height;
 	float game_width;
 
+	float thrust_accel = 1f;
 
-	/* SHIP STUFF */
-	/*
+	/* SHIP STUFF
+
 	Dimensions
 	 */
 	float kship_height;
@@ -50,19 +60,38 @@ public class KatchGame extends ApplicationAdapter implements GestureDetector.Ges
 	
 	@Override
 	public void create () {
-		Gdx.input.setInputProcessor(new GestureDetector(this));
+		Gdx.input.setInputProcessor(this);
 		batch = new SpriteBatch();
 		assets = new AssetManager();
 		assets.load("kship.pack", TextureAtlas.class);
 		assets.finishLoading();
-
-		game_height = Gdx.graphics.getHeight();
-		game_width = Gdx.graphics.getWidth();
+		/*
+		assign sprites from the texture atlas
+		 */
 
 		kshipAtlas = assets.get("kship.pack");
 		kshipSprites = kshipAtlas.getRegions();
 
+		sprite_r = kshipSprites.get(0);
+		sprite_lr = kshipSprites.get(1);
+		sprite_n = kshipSprites.get(3);
+		sprite_l = kshipSprites.get(4);
+
+		game_height = Gdx.graphics.getHeight();
+		game_width = Gdx.graphics.getWidth();
+
 		camera = new OrthographicCamera(game_width,game_height);
+
+		/*
+		generate dimensions based on screen size
+		 */
+		kship_width = Gdx.graphics.getWidth()/5f;
+		kship_height = kship_width * .6f;
+
+		kship_x = Gdx.graphics.getWidth()/2;
+		kship_y = Gdx.graphics.getHeight()/2;
+
+
 
 
 
@@ -70,50 +99,94 @@ public class KatchGame extends ApplicationAdapter implements GestureDetector.Ges
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.5f, .5f, .5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
 
+		updateShip(Gdx.graphics.getDeltaTime());
+		batch.begin();
+		batch.draw(sprite_current,kship_x,kship_y,kship_width/2f,kship_height/2f,kship_width,kship_height,1,1,kship_r);
 		batch.end();
 	}
 
+
+	private void updateShip(float deltaTime){
+		float radians = kship_r * ((float) Math.PI) / 180f;
+		float thrustdx = 0;
+		float thrustdy = 0;
+		sprite_current = sprite_n;
+
+		if (isRightThrusting && isLeftThusting){
+			thrustdx = -thrust_accel * ((float) Math.sin(radians));
+			thrustdy = (thrust_accel * ((float) Math.cos(radians)));
+			sprite_current = sprite_lr;
+		} else if (isLeftThusting || isRightThrusting){
+			sprite_current = (isLeftThusting) ? sprite_l : sprite_r;
+			kship_rv += ((isLeftThusting) ? -thrust_accel : thrust_accel)*deltaTime; //calculate new rotational velocity
+			thrustdx = -(thrust_accel / 7f) * ((float) Math.sin(radians));
+			thrustdy = ((thrust_accel / 7f) * ((float) Math.cos(radians)));
+		}
+
+		kship_xv += thrustdx * deltaTime; //calculate new horizontal velocity
+		kship_yv += thrustdy * deltaTime; //calculate new vertical velocity
+
+		//Apply new velocities to ship position and rotation
+		kship_y += kship_yv;
+		kship_x += kship_xv;
+		kship_r += kship_rv;
+
+	}
+
 	@Override
-	public boolean touchDown(float x, float y, int pointer, int button) {
+	public boolean keyDown(int keycode) {
 		return false;
 	}
 
 	@Override
-	public boolean tap(float x, float y, int count, int button) {
+	public boolean keyUp(int keycode) {
 		return false;
 	}
 
 	@Override
-	public boolean longPress(float x, float y) {
+	public boolean keyTyped(char character) {
 		return false;
 	}
 
 	@Override
-	public boolean fling(float velocityX, float velocityY, int button) {
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if (screenX < (Gdx.graphics.getWidth()/2)){
+			// Touched left side of screen
+			isLeftThusting = true;
+		} else {
+			// Touched right side of screen
+			isRightThrusting = true;
+		}
 		return false;
 	}
 
 	@Override
-	public boolean pan(float x, float y, float deltaX, float deltaY) {
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if (screenX < (Gdx.graphics.getWidth()/2)){
+			// Stopped touching left side of screen
+			isLeftThusting = false;
+		} else {
+			// Stopped touching right side of screen
+			isRightThrusting = false;
+		}
 		return false;
 	}
 
 	@Override
-	public boolean panStop(float x, float y, int pointer, int button) {
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		return false;
 	}
 
 	@Override
-	public boolean zoom(float initialDistance, float distance) {
+	public boolean mouseMoved(int screenX, int screenY) {
 		return false;
 	}
 
 	@Override
-	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+	public boolean scrolled(int amount) {
 		return false;
 	}
 }
